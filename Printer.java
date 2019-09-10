c9e3299865cd32a4609b0bf791480b918891897a
Maxwell-Bickley <55039725+Maxwell-Bickley@users.noreply.github.com>
2019-09-10 15:21:14 -0400

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -1,6 +1,6 @@
 /**
 * This collects any given text from Scan, compares it using regular expressions, filters out repeating patterns, and tallies every time a unique pattern successfully matches the pattern$
-it is being compared to. Then, it stores them into a HashMap, can optionally extract and print the usernames and IP Addresses from the HashMap, and print the lines of code parsed along wit$
+it is being compared to. Then, it stores them into a HashMap, can optionally extract and print the usernames and IP Addresses from the HashMap, and print the lines of code parsed along with$
 unqiue IP Addresses and the number of unique usernames.
 *@author Max Bickley
 *@version 1.0
